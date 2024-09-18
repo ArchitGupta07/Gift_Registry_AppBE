60349f9c006c29e191f945aeae6c465ff4871643
ArchitGupta07 <gupta.archit01@gmail.com>
2024-09-18 14:18:08 +0530

    commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { GiftsModule } from './gifts/gifts.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, GiftsModule],
   controllers: [AppController],
   providers: [AppService],
 })
