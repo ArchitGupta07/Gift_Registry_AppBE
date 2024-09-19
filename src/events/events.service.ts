431e093aee5a878d32d37b972c9916eae85fb1bb
ArchitGupta07 <gupta.archit01@gmail.com>
2024-09-19 12:56:16 +0530

    commit

--- src/events/events.service.ts
+++ src/events/events.service.ts
@@ -4,78 +4,84 @@ import { DatabaseService } from 'src/database/database.service';
 
 @Injectable()
 export class EventsService {
-    constructor(private readonly databaseService : DatabaseService){};
+  constructor(private readonly databaseService: DatabaseService) {}
 
-    async createEvent( createEvent: CreateEventDto) {
-        try {
-            const { userId,eventName, description, organizers, members }: CreateEventDto = createEvent;
-            const event = await this.databaseService.event.create({
-                data: {
-                    userId,
-                    eventName,
-                    description,
-                }
-            });
-            const eventId = event.id;
-            // now add the mapping in the userEvent table
-            let userEvents = [
-                ...organizers.map(userId => ({
-                    userId,
-                    eventId,
-                    role : 'organizer'
-                })),
-                ...members.map(userId => ({
-                    userId,
-                    eventId,
-                    role : 'member'
-                }))
-            ];
-            await this.databaseService.userEvents.createMany({
-                data : userEvents
-            }) 
-            return event;
-        } catch (error) {
-            console.error('Error creating event:', error);
-            return null;
-        }
+  async createEvent(createEvent: CreateEventDto) {
+    try {
+      const {
+        userId,
+        eventName,
+        description,
+        organizers,
+        members,
+      }: CreateEventDto = createEvent;
+      const event = await this.databaseService.event.create({
+        data: {
+          userId,
+          eventName,
+          description,
+        },
+      });
+      const eventId = event.id;
+      // now add the mapping in the userEvent table
+      let userEvents = [
+        ...organizers.map((userId) => ({
+          userId,
+          eventId,
+          role: 'organizer',
+        })),
+        ...members.map((userId) => ({
+          userId,
+          eventId,
+          role: 'member',
+        })),
+      ];
+      await this.databaseService.userEvents.createMany({
+        data: userEvents,
+      });
+      return event;
+    } catch (error) {
+      console.error('Error creating event:', error);
+      return null;
     }
-    async getEvents(userId : Number) {
-        try {
-            const userEvents = await this.databaseService.userEvents.findMany({
-              where: {
-                userId: userId,
-              },
-              select: {
-                eventId: true,
-                role : true
-              },
-            });
-      
-            const eventIds: number[] = userEvents.map(
-              (event: { eventId: any }) => event.eventId
-            );
-      
-            const events = await this.databaseService.event.findMany({
-              where: {
-                id: {
-                  in: eventIds,
-                },
-              },
-            });
-      
-                  // for every event i also want to send the type of event for that specific event
-                  const eventsWithRoles = events.map(event => {
-                      const userEvent = userEvents.find(ue => ue.eventId === event.id);
-                      return {
-                        ...event,
-                      role: userEvent?.role
-                      };
-                    });
-                
-                    return eventsWithRoles;
-              } catch (error) {
-                  console.error('Error fetching shared events:', error);
-                  return [];
-              }
+  }
+  async getEvents(userId: number) {
+    try {
+      const userEvents = await this.databaseService.userEvents.findMany({
+        where: {
+          userId,
+        },
+        select: {
+          eventId: true,
+          role: true,
+        },
+      });
+
+      const eventIds: number[] = userEvents.map(
+        (event: { eventId: any }) => event.eventId,
+      );
+
+      const events = await this.databaseService.event.findMany({
+        where: {
+          id: {
+            in: eventIds,
+          },
+        },
+      });
+
+      // for every event i also want to send the type of event for that specific event
+      const eventsWithRoles = events.map((event) => {
+        const userEvent = userEvents.find((ue) => ue.eventId === event.id);
+        return {
+          ...event,
+          role: userEvent?.role,
+        };
+      });
+
+      return eventsWithRoles;
+    } catch (error) {
+      console.error('Error fetching shared events:', error);
+      return [];
     }
+  }
 }
