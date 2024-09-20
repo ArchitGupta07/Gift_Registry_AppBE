01c11b7182f9478e35cefd506d52114e5dcb0572
thepandeyop <thepandeyop8@gmail.com>
2024-09-20 12:41:19 +0530

    adding redirect to page

--- src/Auth/auth.controller.ts
+++ src/Auth/auth.controller.ts
@@ -1,25 +1,40 @@
-import { Controller, UseGuards,Get,Post, Req } from "@nestjs/common";
-import { authService } from "./auth.service";
-import { AuthGuard } from "@nestjs/passport";
-import {Request} from "express";
-import { AuthDto } from "./dto/auth.dto";
+import { Controller, Get, UseGuards, Req, Res, HttpStatus } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Request, Response } from 'express';
+import { authService } from './auth.service'; // Correct import statement
+import { AuthDto } from './dto/auth.dto';
+
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: authService) {}
+  constructor(private readonly AuthService: authService) {} // Correct constructor parameter name
 
   @Get('google')
   @UseGuards(AuthGuard('google'))
   async googleAuth(@Req() req: Request) {
+    // Passport handles the redirection to Google's login page
   }
 
   @Get('google/callback')
   @UseGuards(AuthGuard('google'))
-  async googleAuthRedirect(@Req() req: any) {
-    
-    const user = await this.authService.ValidateOrCreateUser(
-      req.user.user.googleId,
-      req.user.user
-    );
-    return new AuthDto(user);
+  async googleAuthRedirect(@Req() req: any, @Res() res: Response) {
+    try {
+      // Validate or create user
+      const userReq = req.user;
+      const user = await this.AuthService.ValidateOrCreateUser(
+        userReq.user.googleId,
+        userReq.user
+      );
+
+      // User data to send
+      const userDto = new AuthDto(user);
+
+      // Redirect to the profile page with user data as query parameter
+      const redirectUrl = `http://localhost:3000/dashboard?user=${encodeURIComponent(JSON.stringify(userDto))}`;
+      console.log(userDto.profilePic,userDto.email,userDto.username);
+      res.redirect(redirectUrl);
+    } catch (error) {
+      console.error('Error handling Google callback:', error);
+      res.status(HttpStatus.INTERNAL_SERVER_ERROR).send('Internal Server Error');
+    }
   }
 }
--- src/Auth/auth.service.ts
+++ src/Auth/auth.service.ts
@@ -2,12 +2,15 @@ import { Injectable } from "@nestjs/common";
 import {AuthDto} from './dto/auth.dto';
 import { DatabaseService } from "src/database/database.service";
 
+
 @Injectable()
 export class authService{
 
     constructor(private readonly databaseService : DatabaseService) {}
 
     async ValidateOrCreateUser(googleId: string,userDto: AuthDto){
+        console.log(googleId);
+    
         const user = await this.databaseService.user.findUnique({
             where: {googleId},
         });
@@ -21,7 +24,9 @@ export class authService{
                     profilePic: userDto.profilePic,
                 },
             });
+        
         }
+        console.log(user.profilePic);
 return user;
     }
    
