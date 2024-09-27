cf260af29c081f2de8ad57b49bcdce362c6321f9
ArchitGupta07 <gupta.archit01@gmail.com>
2024-09-27 12:13:08 +0530

    commit

--- src/Auth/auth.service.ts
+++ src/Auth/auth.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, BadRequestException, InternalServerErrorException } from '@nestjs/common';
+import {
+  Injectable,
+  BadRequestException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { AuthDto } from './dto/auth.dto';
 import { JwtService } from '@nestjs/jwt'; // Correcting the import for JwtService
 import { DatabaseService } from 'src/database/database.service';
@@ -7,19 +11,16 @@ import { DatabaseService } from 'src/database/database.service';
 export class AuthService {
   constructor(
     private readonly databaseService: DatabaseService,
-    private readonly jwtService: JwtService, 
+    private readonly jwtService: JwtService,
   ) {}
 
- 
   async ValidateOrCreateUser(googleId: string, userDto: AuthDto) {
     try {
       console.log(googleId);
 
-      
       const user = await this.databaseService.user.findUnique({
         where: { googleId },
       });
-
 
       if (!user) {
         const newUser = await this.databaseService.user.create({
@@ -31,24 +32,27 @@ export class AuthService {
           },
         });
 
-
         const token = this.generateJwtToken(newUser);
 
         return { user: newUser, token };
       }
 
-
       const token = this.generateJwtToken(user);
       return { user, token };
     } catch (error) {
       console.error('Error in ValidateOrCreateUser:', error);
-      throw new InternalServerErrorException('Failed to validate or create user');
+      throw new InternalServerErrorException(
+        'Failed to validate or create user',
+      );
     }
   }
 
-  
-   generateJwtToken(user: any) {
-    const payload = { userId: user.id, username: user.username, email: user.email };
+  generateJwtToken(user: any) {
+    const payload = {
+      userId: user.id,
+      username: user.username,
+      email: user.email,
+    };
     return this.jwtService.sign(payload);
   }
 }
