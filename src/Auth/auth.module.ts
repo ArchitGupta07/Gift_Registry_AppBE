60041432e95d05a0bf4e195043530ea890752f20
ArchitGupta07 <gupta.archit01@gmail.com>
2024-09-27 12:09:14 +0530

    error fixed

--- src/Auth/auth.module.ts
+++ src/Auth/auth.module.ts
@@ -1,22 +1,24 @@
-import { Module } from "@nestjs/common";
-import { AuthController } from "./Auth.controller";
-import { AuthService } from "./auth.service";
-import { GoogleStrategies } from "./strategies/google.strategies";
-import { PassportModule } from "@nestjs/passport";
-import { DatabaseModule } from "src/database/database.module";
-import { UserModule } from "src/users/users.module"
-import { JwtModule } from "@nestjs/jwt";
+import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { GoogleStrategies } from './strategies/google.strategies';
+import { PassportModule } from '@nestjs/passport';
+import { DatabaseModule } from 'src/database/database.module';
+import { UserModule } from 'src/users/users.module';
+import { JwtModule } from '@nestjs/jwt';
 import * as dotenv from 'dotenv';
 dotenv.config();
 @Module({
-    imports: [PassportModule,DatabaseModule , UserModule, JwtModule.register({
-        secret:process.env.JWT_SECRET, 
-        signOptions: { expiresIn: '1h' }, 
-      }),],
-    controllers: [AuthController],
-    providers:[AuthService,GoogleStrategies],
+  imports: [
+    PassportModule,
+    DatabaseModule,
+    UserModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, GoogleStrategies],
 })
-export class AuthModule
-{
-
-}
+export class AuthModule {}
