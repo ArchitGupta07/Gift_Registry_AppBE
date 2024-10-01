abf275fc2e317e5faa3accdd29925675132759da
codeshekh <pandeyabhishek1518@gmail.com>
2024-10-01 10:23:18 +0530

    several changes in the main.ts

--- src/Auth/dto/auth.dto.ts
+++ src/Auth/dto/auth.dto.ts
@@ -1,12 +1,14 @@
-
 export class AuthDto {
+  id: number;
     username: string;
     email: string;
     profilePic: string;
-
+  
     constructor(user: any) {
-        this.username = user.username;
-        this.email = user.email;
-        this.profilePic = user.profilePic;
+      this.id = user.id
+      this.username = user.username; // Set the correct user fields
+      this.email = user.email;
+      this.profilePic = user.profilePic;
     }
-}
+  }
+  
