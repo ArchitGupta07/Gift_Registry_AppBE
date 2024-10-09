b234e0317bee65b077ad6f9191af8fd62aba4c6a
ArchitGupta07 <gupta.archit01@gmail.com>
2024-10-09 17:02:03 +0530

    web socket integrated

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,12 +10,14 @@ import { RegistriesModule } from './registries/registries.module';
 import { GroupsModule } from './groups/groups.module';
 import { ConfigModule } from '@nestjs/config';
 import { ChatModule } from './chat/chat.module';
+import { GatewayModule } from './gateway/gateway.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     UserModule,
     AuthModule,
+    GatewayModule,
     // GiftsModule,
     
     forwardRef(() => DatabaseModule),
---
+++ src/gateway/gateway.ts
@@ -0,0 +1,43 @@
+
+import { OnModuleInit } from '@nestjs/common';
+import { WebSocketGateway, WebSocketServer, SubscribeMessage, MessageBody, ConnectedSocket } from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+
+
+
+@WebSocketGateway()
+export class MyGateway implements OnModuleInit{
+
+  @WebSocketServer() 
+  server: Server;
+
+  onModuleInit() {
+      this.server.on('connection',(socket)=>{
+        console.log(socket.id)
+        console.log("Web socket Connected")
+      })
+  }
+
+
+  handleConnection(socket: Socket) {
+    console.log(`Client connected: ${socket.id}`);
+    this.server.emit('onConnect', { message: `Welcome client: ${socket.id}` });
+  }
+
+  handleDisconnect(socket: Socket) {
+    console.log(`Client disconnected: ${socket.id}`);
+  }
+
+  @SubscribeMessage('message')
+  onNewMessage(@MessageBody() body :any, @ConnectedSocket() client: Socket): void {
+    console.log(body)
+    console.log("check web socket")
+
+    this.server.emit('onMessage',{
+        msg:'new message',
+        content:body,
+        sender: client.id
+    })
+    // this.server.emit('message', payload);
+  }
+}
